e2075b1b0b724fba5f87fd03e123053fe76eeebe
4m4r199707 <34017886+4m4r199707@users.noreply.github.com>
2020-05-04 14:56:03 +0530

    Updates

---
+++ precache-manifest.8f700ffdea1698ac64466575e0d9bc02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd91d00aa834e1eceb9b4b84c755960f",
+    "url": "/Monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1abb300d6d7190aa0919",
+    "url": "/Monsters-rolodex/static/css/main.4e398d8d.chunk.css"
+  },
+  {
+    "revision": "79ad37e630052ae7a401",
+    "url": "/Monsters-rolodex/static/js/2.2c32a551.chunk.js"
+  },
+  {
+    "revision": "1abb300d6d7190aa0919",
+    "url": "/Monsters-rolodex/static/js/main.07fce1ac.chunk.js"
+  },
+  {
+    "revision": "142ad0950a4be7d96194",
+    "url": "/Monsters-rolodex/static/js/runtime~main.973861d2.js"
+  }
+]);
